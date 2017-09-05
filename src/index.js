c00fd13d01194a56a0d39b5ed583f4f0cb654394
JBeCast <jbcsound@gmail.com>
2017-09-05 23:45:22 +0200

    Changed hardcoded squares for two nested loops.

--- src/index.js
+++ src/index.js
@@ -23,25 +23,20 @@ class Board extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
+    return <div>{(() => {
+        let rows = [];
+        for (let i=0;i<3;i++) {
+          rows.push(<div className="board-row">{(() => {
+            let squares = [];
+            for (let j=0;j<3;j++) {
+              squares.push(this.renderSquare(i*3+j));
+            }
+            return squares;})()
+          }</div>);
+        }
+        return rows;
+      })()}
+    </div>
   }
 }
 
